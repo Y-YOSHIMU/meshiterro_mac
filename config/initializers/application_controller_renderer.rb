6cc39c29a81965c81ed75133e6661693abd01cb0
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2020-07-05 16:11:29 +0900

    [Update]4章まで

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '293c3b87c457ae25ef8a8eaf796eb6b4c01121d151f0133aa1fa5a2334a1a280717a33692d8fe7d179e61aa9b0bf5a48e10cb696c6908d2b0013d462790a885c'
